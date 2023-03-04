02b2f45a22941b95de87929eeef4b372fd631619
elizeuangelo <elizeu90@gmail.com>
2023-03-04 13:53:47 -0300

    initial release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Foundry VTT - 🧼 Chat Cleaner
+
+Automatically sanitizes the chat, for a smoother Foundry experience.
+
+## Installation
+
+In the setup screen, use the manifest URL https://raw.githubusercontent.com/elizeuangelo/fvtt-module-chat-cleaner/master/module.json to install the module.
+
+## How to Use
+
+Just configure the module's settings to your desire! The module automatically:
+
+-   Cleans your chat log, deleting old messages up to your desired maximum
+-   Lazy loads your messages to occupy less memory in the DOM
---
+++ module/settings.ts
@@ -0,0 +1,50 @@
+export const SYSTEM_ID = 'chat-cleaner';
+
+const settings = {
+	sensitiveMessages: {
+		name: 'Exclude Sensitive Chat Messages',
+		hint: 'Also exclude whispers, in-character and emote messages.',
+		scope: 'world',
+		config: true,
+		type: Boolean,
+		default: true,
+	},
+	maxMessages: {
+		name: 'Maximum Messages in Chat Log',
+		hint: 'The maximum messages you want to keep in the chat log.',
+		scope: 'world',
+		config: true,
+		type: Number,
+		default: 50,
+		range: {
+			min: 10,
+			max: 100,
+			step: 1,
+		},
+	},
+	scrollingMessages: {
+		name: 'Scrolling Buffer',
+		hint: 'How many messages you want to preload in the chat log. Scrolling loads more messages.',
+		scope: 'world',
+		config: true,
+		type: Number,
+		default: 10,
+		range: {
+			min: 1,
+			max: 100,
+			step: 1,
+		},
+	},
+} as const;
+
+export type Settings = typeof settings;
+
+export function getSetting<T extends keyof Settings>(name: T) {
+	return game.settings.get(SYSTEM_ID, name) as unknown as ReturnType<Settings[T]['type']>;
+}
+
+Hooks.once('setup', () => {
+	for (const [key, setting] of Object.entries(settings)) {
+		game.settings.register(SYSTEM_ID, key, setting as unknown as any);
+	}
+});
